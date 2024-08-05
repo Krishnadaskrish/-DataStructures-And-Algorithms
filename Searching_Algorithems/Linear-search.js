2a0c60d1568ba39c002d9a4b59921cd7a9cf0452
hornto <krishnadas10.official@gmail.com>
2024-08-05 13:09:54 +0530

    first commit

---
+++ Searching_Algorithems/Linear-search.js
@@ -0,0 +1,15 @@
+// Linear Search
+
+function LinearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+const arr = [3,5,3,5,7,89,3]
+const target = 7
+console.log(LinearSearch(arr,target))
